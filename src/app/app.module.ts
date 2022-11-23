54ce83aec59455c4314323bf5ad34b67579383bf
smeraz1990 <smeraz@genomi-k.com>
2022-11-23 16:22:28 -0600

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuLatComponent } from './menu-lat/menu-lat.component';
+import { ContentComponent } from './content/content.component';
+import { MenuSupComponent } from './menu-sup/menu-sup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuLatComponent,
+    ContentComponent,
+    MenuSupComponent
   ],
   imports: [
     BrowserModule,
